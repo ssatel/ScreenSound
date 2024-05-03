ed0ef3b48eebd0b73f7ec51dcfd4474f495ca0fb
saulo.satel <saulo.satel@gmail.com>
2024-05-03 20:36:41 -0300

    Aula 02 - Tratanto Erros

--- ScreenSound/ScreenSound/Program.cs
+++ ScreenSound/ScreenSound/Program.cs
@@ -1,7 +1,22 @@
 ﻿using(HttpClient client =  new HttpClient())
 {
-    string listaMusicas = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+	var testeDeuBom = false;
+	var endPointCorreto = "https://guilhermeonrails.github.io/api-csharp-songs/songs.json";
+    var endPointQuebrado = "https://guilhermeonrails.github.io/api-csharp-songs/songs.jso";
 
-    Console.WriteLine(listaMusicas);
+	var endPoint = testeDeuBom? endPointCorreto: endPointQuebrado;
+
+    try
+	{
+
+		string listaMusicas = await client.GetStringAsync(endPoint);
+
+		Console.WriteLine(listaMusicas);
+
+	}
+	catch (Exception ex)
+	{
+		Console.WriteLine("Sinto muito deu ruim ... olha o que aconteceu ..." + ex.Message.ToString());
+	}
 
 }
