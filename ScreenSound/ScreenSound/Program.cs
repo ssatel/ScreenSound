aaa7ac352d448a1a845d999332db6731c060cb34
saulo.satel <saulo.satel@gmail.com>
2024-05-03 20:11:13 -0300

    SetUp Estudo httpClient - Aula 01

---
+++ ScreenSound/ScreenSound/Program.cs
@@ -0,0 +1,7 @@
+﻿using(HttpClient client =  new HttpClient())
+{
+    string listaMusicas = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+
+    Console.WriteLine(listaMusicas);
+
+}
