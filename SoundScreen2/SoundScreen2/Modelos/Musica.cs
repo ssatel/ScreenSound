b3a279d0745311a5a368ad1c2d4e77479dc3c6fe
saulo.satel <saulo.satel@gmail.com>
2024-05-04 20:25:36 -0300

    Permitir Genero Nulo

---
+++ SoundScreen2/SoundScreen2/Modelos/Musica.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SoundScreen2.Modelos
+{
+    public class Musica
+    {
+        [JsonPropertyName("song")]
+        public string? Nome { get; set; }
+
+        [JsonPropertyName("artist")]
+        public string? Artista { get; set; }
+        [JsonPropertyName("duration_ms")]
+        public int Duracao {get; set; }
+
+        [JsonPropertyName("genre")]
+        public string? Genero { get; set; }
+
+        public void ExibirDetalhes()
+        {
+            Console.WriteLine($"Gênero: {Genero}");
+            Console.WriteLine($"Artista: {Artista}");
+            Console.WriteLine($"Nome da Musica: {Nome}");
+            Console.WriteLine($"Duracao em segundos: {Duracao / 1000}");
+        }
+    }
+}
