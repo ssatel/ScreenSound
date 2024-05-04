3c76b10c3f647aeed2faab9332e3b0ac8cdd6d28
saulo.satel <saulo.satel@gmail.com>
2024-05-03 21:51:34 -0300

    Tratar erro na busca de item da lista

---
+++ MostraItemListaInteiros/MostraItemListaInteiros/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+
+Console.WriteLine("Lista de Inteiros");
+
+var listainteiros = new List<int>()
+{
+    1,2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+};
+
+try
+{
+
+    Console.WriteLine("Mostrar o item: ");
+    var inputNumero = Console.ReadLine();
+
+    var numeroItem = 0;
+
+    if (int.TryParse(inputNumero, out int result))
+    {
+        numeroItem = result;
+    }
+
+    var itemLista = listainteiros.IndexOf(numeroItem);
+
+    Console.WriteLine(itemLista.ToString());
+
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Não foi ..." + ex.Message.ToString());
+}
+
+foreach (var item in listainteiros)
+{
+    Console.WriteLine(item);
+}
