2eff833b5c24e99265032251cbb0d0a33bdab47a
saulo.satel <saulo.satel@gmail.com>
2024-05-04 20:23:34 -0300

    Criar a classe musica

---
+++ SoundScreen2/SoundScreen2/Program.cs
@@ -0,0 +1,16 @@
+﻿using (HttpClient client = new HttpClient())
+{
+	try
+	{
+
+		var resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+
+		Console.WriteLine(resposta);
+
+	}
+	catch (Exception ex)
+	{
+		Console.WriteLine("Houve um problema..." + ex.Message.ToString());
+	}
+
+}
