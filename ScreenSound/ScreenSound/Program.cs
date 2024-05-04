15853a69e13a3de89f8cde5a58ebbbb48b52d412
saulo.satel <saulo.satel@gmail.com>
2024-05-03 21:06:16 -0300

    URL Games

--- ScreenSound/ScreenSound/Program.cs
+++ ScreenSound/ScreenSound/Program.cs
@@ -1,22 +1,32 @@
-﻿using(HttpClient client =  new HttpClient())
+﻿using System.Net;
+using System.Runtime.CompilerServices;
+
+using (HttpClient client =  new HttpClient())
 {
 	var testeDeuBom = true;
 	var endPointCorreto = "https://guilhermeonrails.github.io/api-csharp-songs/songs.json";
     var endPointQuebrado = "https://guilhermeonrails.github.io/api-csharp-songs/songs.jso";
+	var endPointGames = "https://www.cheapshark.com/api/1.0/deals";
 
-	var endPoint = testeDeuBom? endPointCorreto: endPointQuebrado;
 
-    try
+    var endPoint = testeDeuBom? endPointCorreto: endPointQuebrado;
+
+	Console.WriteLine("Entre a função:");
+	string funcao = Console.ReadLine();
+
+	if (funcao == "1")
 	{
+		try
+		{
+			string listaMusicas = await client.GetStringAsync(endPoint);
+			Console.WriteLine(listaMusicas);
 
-		string listaMusicas = await client.GetStringAsync(endPoint);
-
-		Console.WriteLine(listaMusicas);
-
-	}
-	catch (Exception ex)
-	{
-		Console.WriteLine("Sinto muito deu ruim ... olha o que aconteceu ..." + ex.Message.ToString());
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine("Sinto muito deu ruim ... olha o que aconteceu ..." + ex.Message.ToString());
+		}
 	}
 
 }
+
