1156305f52537199f95d7d6a9456b55f110e6476
saulo.satel <saulo.satel@gmail.com>
2024-05-04 18:52:52 -0300

    Apis Publicas para testar

---
+++ CatAoiScreenConsole/CatAoiScreenConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using (HttpClient client = new HttpClient())
+{
+	try
+	{
+		var cats = await client.GetStringAsync("https://cataas.com/api/tags");
+
+		Console.WriteLine(cats);
+
+	}
+	catch (Exception ex)
+	{
+		Console.WriteLine(ex.ToString());
+		
+	}
+
+}
